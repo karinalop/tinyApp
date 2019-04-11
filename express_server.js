67928c1ea31af71668d47ffc7b4c90d5a03209a8
karinalop <karina.popeye@gmail.com>
2019-04-11 00:50:23 +0000

    Add logout functionality

--- express_server.js
+++ express_server.js
@@ -55,7 +55,8 @@ app.get("/urls", (req, res) => {
 
 //Display page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -115,6 +116,11 @@ app.post("/urls/:shortURL", (req,res) => {
 app.post("/login",  (req, res) =>{
   //sets a cookie object{username: "username"}
   res.cookie("username",req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout",  (req, res) =>{
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
